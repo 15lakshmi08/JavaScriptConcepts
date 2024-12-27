0e30628f983484980a84d61d5d9f359f3271a907
15lakshmi08 <lakshmi150803@gmail.com>
2024-12-27 18:54:58 +0530

    Summary of DataTypes

---
+++ 01_Basics/DataTypes_Summary.js
@@ -0,0 +1,35 @@
+// Primitive DataTypes--------------------
+// There are 7 types of primitive datatype---
+
+// String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+let s1=1;
+let s2=1.2;
+let sign=false;
+let s3=null;
+let a;
+const big=2333333339993333;
+
+let id=Symbol("412");
+let id2=Symbol("412");
+
+console.log(id==id2);
+
+
+
+// Reference Type or (Non Primitive DataType)-----------
+
+// Array,Objects,Functions
+
+
+let god=["Ram","Shyam","Shiva"];
+
+let Objects={
+    name:"Lakshmi",
+    age:20,
+}
+
+const Fnc=function(){
+    console.log("Lakshmi Singh");
+}
+console.log(typeof Objects);
