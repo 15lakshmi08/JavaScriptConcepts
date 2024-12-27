43029f3a75b28468329679691babd0d2e85df5a1
15lakshmi08 <lakshmi150803@gmail.com>
2024-12-27 18:15:58 +0530

    Basic Operations

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -1,20 +1,45 @@
-let num="23aaa";
+// Conversion-------------------------------------
 
-// two method of checking typeof
-console.log(typeof num);
-console.log(typeof(num));
+// let num="23aaa";
 
-// explicit conversion which convert string into number
-console.log(typeof Number(num));
+// // two method of checking typeof
+// console.log(typeof num);
+// console.log(typeof(num));
 
-// If there is "23" it will be convert into 23 but "23aaa" can't . It return NaN
-// true convert into 1 and false convert into 0    true=>1 and false=>0   
-console.log(Number(num));
-console.log(Boolean(num));
-console.log(String(num));
+// // explicit conversion which convert string into number
+// console.log(typeof Number(num));
+
+// // If there is "23" it will be convert into 23 but "23aaa" can't . It return NaN
+// // true convert into 1 and false convert into 0    true=>1 and false=>0
+// console.log(Number(num));
+// console.log(Boolean(num));
+// console.log(String(num));
+
+// // It pass only number not alphabet
+// console.log(parseInt(num));
+
+// Opertions-------------------------------------
+
+let s1 = "Hello";
+let s2 = " Lakshmi";
+
+// console.log(s1 + s2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(false);
+console.log(+true);
+console.log(+"");
 
 
-// It pass only number not alphabet
-console.log(parseInt(num));
+let n1, n2, n3;
+n1 = n2 = n3 = 2 + 2;
 
 
+let count=12;
+// console.log(++count);
+console.log(count++);
