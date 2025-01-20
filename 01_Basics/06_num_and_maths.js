0f1bcbd94e959454944956a19b95d38c2c08cb1d
15lakshmi08 <lakshmi150803@gmail.com>
2025-01-20 14:16:54 +0530

    Numbers in javascript

---
+++ 01_Basics/06_num_and_maths.js
@@ -0,0 +1,23 @@
+// .....................Number..............................
+
+const score=400;
+console.log(score);
+
+const balance=new Number(400);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anyNumber=213.579;
+console.log(anyNumber.toPrecision(3));
+
+const hundreds=10000;
+console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+
+
+// ......................Maths...............................
