fc236599f5a75fd05c44b0f008613d62b9489735
15lakshmi08 <lakshmi150803@gmail.com>
2025-01-20 14:36:13 +0530

    Maths in Javascript

--- 01_Basics/06_num_and_maths.js
+++ 01_Basics/06_num_and_maths.js
@@ -1,23 +1,43 @@
 // .....................Number..............................
 
-const score=400;
-console.log(score);
+// const score=400;
+// console.log(score);
 
-const balance=new Number(400);
-console.log(balance);
+// const balance=new Number(400);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
-const anyNumber=213.579;
-console.log(anyNumber.toPrecision(3));
+// const anyNumber=213.579;
+// console.log(anyNumber.toPrecision(3));
 
-const hundreds=10000;
-console.log(hundreds.toLocaleString("en-IN"));
+// const hundreds=10000;
+// console.log(hundreds.toLocaleString("en-IN"));
 
 
 
 
 
 // ......................Maths...............................
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,9,6,8));
+// console.log(Math.max(4,9,6,8));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+
+
+const minimum=10;
+const maximum=20;
+
+console.log(Math.floor(Math.random()*(maximum-minimum+1)+minimum));
