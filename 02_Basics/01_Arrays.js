0acf836cd444984d636e92b0d6564b89b9411175
15lakshmi08 <lakshmi150803@gmail.com>
2025-01-22 22:03:44 +0530

    Basic of Array

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,11 @@
+// Basics of Array
+
+const arr=[8, 9, 10, 11, 12];
+console.log(arr[0]);
+
+const newArr=["Lakshmi","Manu","Radhey"];
+console.log(newArr[2]);
+
+
+const Arr1=new Array(2,5,8,6);
+console.log(Arr1[3]);
