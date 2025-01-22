48aa993493a03d8a78b3c2fd13adf4bc929ea16a
15lakshmi08 <lakshmi150803@gmail.com>
2025-01-22 23:12:37 +0530

    Array Methods

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,38 @@
+const arr1=[1,2,3];
+const arr2=[4,5,6];
+
+
+// Push method 
+
+// arr1.push(arr2);
+// console.log(arr1);
+// console.log(arr1[3][1]);
+
+
+// Concatination
+
+// const arr3=arr1.concat(arr2);
+// console.log(arr3);
+
+
+// Spread Operator
+
+// const arr3=[...arr1,...arr2];
+// console.log(arr3);
+
+
+
+// flat method
+
+// let arr=[1,2,3,[4,5],[6,[7,8],9],0]
+// console.log(arr);
+// console.log(arr.flat(Infinity));
+
+
+// console.log(Array.isArray("Lakshmi"));
+// console.log(Array.from("Lakshmi"));
+// console.log(Array.from({name:"Lakshmi"}));
+
+let s1=2,s2=3,s3=4;
+
+console.log(Array.of(s1,s2,s3));
