3ff4de97c20d9646a74074a584937845e48c867c
15lakshmi08 <lakshmi150803@gmail.com>
2025-03-05 17:38:26 +0530

    Control flow in javascript

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const week = "sunday"
+
+switch (week) {
+    case "sunday":
+        console.log("Sunday");
+        break;
+    case "monday":
+        console.log("Monday");
+        break;
+    case "tuesday":
+        console.log("Tuesday");
+        break;
+    case "wednesday":
+        console.log("Wednesday");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
